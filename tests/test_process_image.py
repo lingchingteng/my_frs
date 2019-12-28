d89d6e91fbaeddbeba109564096bfab790224cef
CT_Ling <xxxxcccvvvbbb@gmail.com>
2019-12-28 22:54:11 +0800

    [TestCase] rename testset name

--- tests/test_process_image.py
+++ tests/test_process_image.py
@@ -5,10 +5,10 @@ from main import process_image
 
 
 def test_process_image():
-    path_image = "tests/testset/input.jpg"
+    path_image = "tests/testset/seven_people_image.jpg"
 
     # Clear output folder
-    path_output_folder = "output/input"
+    path_output_folder = "output/seven_people_image"
 
     if os.path.exists(path_output_folder):
         shutil.rmtree(path_output_folder)
@@ -18,18 +18,26 @@ def test_process_image():
 
     # check reuslts
     assert os.path.exists(path_output_folder)
-    assert os.path.exists(os.path.join(path_output_folder, "input.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_bbox.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_face_0.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_face_1.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_face_2.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_face_3.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_face_4.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_face_5.jpg"))
-    assert os.path.exists(os.path.join(path_output_folder, "input_face_6.jpg"))
-
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_bbox.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_face_0.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_face_1.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_face_2.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_face_3.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_face_4.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_face_5.jpg"))
+    assert os.path.exists(
+        os.path.join(path_output_folder, "seven_people_image_face_6.jpg"))
     assert not os.path.exists(
-        os.path.join(path_output_folder, "input_face_7.jpg"))
+        os.path.join(path_output_folder, "seven_people_image_face_7.jpg"))
 
     if os.path.exists(path_output_folder):
         shutil.rmtree(path_output_folder)
