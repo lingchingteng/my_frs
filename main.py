bee3544e56e6f3a12230d48aa21586d66e22d942
CT_Ling <xxxxcccvvvbbb@gmail.com>
2019-12-28 11:41:03 +0800

    refactor main.py

--- main.py
+++ main.py
@@ -1,85 +1,50 @@
 import os
-import shutil
 from argparse import ArgumentParser
 
 import cv2
 from mtcnn.mtcnn import MTCNN
 
+from utils import copy_input_to_database
+
 detector = MTCNN()
-
-
-def copy_input_to_database(path_input):
-    output_folder = "output"
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    basename = os.path.basename(path_input)
-    filename, ext = os.path.splitext(basename)
-    path_output_folder = os.path.join(output_folder, filename)
-    if not os.path.exists(path_output_folder):
-        os.makedirs(path_output_folder)
-
-    shutil.copy2(path_input, path_output_folder)
-
-    path_input = os.path.join(path_output_folder, basename)
-
-    return path_input
 
 
 def main(args):
 
-    if ".mp4" == os.path.splitext(args.path_input)[1]:
-        path_input = copy_input_to_database(args.path_input)
+    path_input = copy_input_to_database(args.path_input)
 
-        detect_video(path_input)
+    dets = detect_faces_from_path(path_input)
 
-    elif ".jpg" == os.path.splitext(args.path_input)[1]:
+    save_faces(path_input, dets)
 
-        path_input = copy_input_to_database(args.path_input)
-
-        img = cv2.imread(path_input)
-
-        detect_image(img, path_input, save_bbox=True, save_face=True)
+    draw_bounding_box_and_save(path_input, dets)
 
 
-def detect_video(path_video):
+def draw_bounding_box_and_save(path_image, dets):
 
-    cap = cv2.VideoCapture(path_video)
+    img = cv2.imread(path_image)
 
-    frame_cnt = 0
-    while cap.isOpened():
+    for det in dets:
 
-        ok, frame = cap.read()
+        line_width = min(
+            int(min(img.shape[0:2]) * 0.01),  # Basic width, img too big
+            int(min(det['box'][2], det['box'][3]) * 0.08),  # face too big
+        )
 
-        if not ok:
-            cap.release()
-            break
+        cv2.rectangle(
+            img, (det['box'][0], det['box'][1]),
+            (det['box'][0] + det['box'][2], det['box'][1] + det['box'][3]),
+            (255, 0, 0), line_width)
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        dets = detect_image(frame)
+    filename, ext = os.path.splitext(path_image)
+    path_bbox_image = path_image.replace(ext, "_bbox%s" % (ext, ))
 
-        for idx, det in enumerate(dets):
-
-            ext = os.path.splitext(path_video)[1]
-            path_face = path_video.replace(
-                ext, "frame_%d_face_%d" % (
-                    frame_cnt,
-                    idx,
-                ) + ".jpg")
-
-            cv2.imwrite(
-                path_face,
-                cv2.cvtColor(det['image_cropped_face'], cv2.COLOR_RGB2BGR))
-
-        frame_cnt += 1
+    cv2.imwrite(path_bbox_image, img)
 
 
-def detect_image(img, path_image, save_face=False, save_bbox=False):
+def save_faces(path_image, dets):
 
-    if path_image and save_bbox:
-        bbox_img = img.copy()
-
-    dets = detector.detect_faces(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    img = cv2.imread(path_image)
 
     for idx, det in enumerate(dets):
         bbox = det['box']
@@ -88,33 +53,24 @@ def detect_image(img, path_image, save_face=False, save_bbox=False):
             max(0, bbox[1]):min(img.shape[0] - 1, bbox[1] + bbox[3]),
             max(0, bbox[0]):min(img.shape[1] - 1, bbox[0] + bbox[2])
         ]  # yapf: disable
+
         cropped_face = cv2.resize(cropped_face, (160, 160))
 
-        det['image_cropped_face'] = cropped_face
-
-        if path_image and save_face:
-            ext = os.path.splitext(path_image)[1]
-            path_face = path_image.replace(ext, "_face_%d" % (idx, ) + ext)
-
-            det['path_cropped_face'] = path_face
-
-            cv2.imwrite(path_face, cropped_face)
-
-        if path_image and save_bbox:
-            line_width = min(
-                int(min(img.shape[0:2]) * 0.01),  # Basic width, img too big
-                int(min(det['box'][2], det['box'][3]) * 0.08),  # face too big
-            )
-            cv2.rectangle(
-                bbox_img, (det['box'][0], det['box'][1]),
-                (det['box'][0] + det['box'][2], det['box'][1] + det['box'][3]),
-                (255, 0, 0), line_width)
-
-    if path_image and save_bbox:
         filename, ext = os.path.splitext(path_image)
-        path_face = path_image.replace(ext, "_detect.jpg")
+        path_face = path_image.replace(ext, "_face_%d%s" % (idx, ext))
 
-        cv2.imwrite(path_face, bbox_img)
+        cv2.imwrite(path_face, cropped_face)
+
+
+def detect_faces_from_path(image_path):
+    img = cv2.imread(image_path)
+
+    return detect_faces_from_image(img)
+
+
+def detect_faces_from_image(img):
+
+    dets = detector.detect_faces(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
     return dets
 
---
+++ utils.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+def copy_input_to_database(path_input):
+    output_folder = "output"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    basename = os.path.basename(path_input)
+    filename, ext = os.path.splitext(basename)
+    path_output_folder = os.path.join(output_folder, filename)
+    if not os.path.exists(path_output_folder):
+        os.makedirs(path_output_folder)
+
+    shutil.copy2(path_input, path_output_folder)
+
+    path_input = os.path.join(path_output_folder, basename)
+
+    return path_input
